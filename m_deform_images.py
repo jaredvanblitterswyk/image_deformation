7249be6de6ea440fe09304881519b61d696d66b5
Jared Van Blitterswyk <jared.vanblitterswyk@nist.gov>
2020-02-13 09:59:01 -0500

    Added documentation to code

--- m_deform_images.py
+++ m_deform_images.py
@@ -32,49 +32,46 @@ from PIL import Image
 import numpy.matlib
 from scipy.interpolate import interp2d, griddata
 
-# Test git diff
-
 os.chdir(r"Z:\Experiments\drop_tower\sa5_images")
 # varify the path using getcwd() 
 cwd = os.getcwd() 
 
-filename = cwd+'/im_test.jpg'
+filename = cwd+'/im_test.jpg' # hardcode the location of the figure
 
-img_ref = Image.open(filename)
+img_ref = Image.open(filename) # open image
 
 imarray = np.asarray(img_ref) # convert image to array
 
 Ny, Nx = imarray.shape # find size of image
-imarray = np.reshape(imarray,Nx*Ny)
+imarray = np.reshape(imarray,Nx*Ny) # reshape to vector for griddata interpolation
 
 #%% Up sample image to perform sub-pixel interpolation
-sample_factor = 10
-x_orig_vec = np.linspace(0,Nx,Nx) # original x coordiantes in vector form
-y_orig_vec = np.linspace(0,Ny,Ny) # original y coordiantes in vector form
+sample_factor = 10 # integer scale factor for upsampled image resolution
+x_orig_vec = np.linspace(0,Nx,Nx) # original x coordinates in vector form
+y_orig_vec = np.linspace(0,Ny,Ny) # original y coordinates in vector form
 
-x_orig_mesh,y_orig_mesh = np.meshgrid(x_orig_vec,y_orig_vec)
+x_orig_mesh,y_orig_mesh = np.meshgrid(x_orig_vec,y_orig_vec) # original coordinates in grid form
 
-x_orig_meshV = np.reshape(x_orig_mesh,Nx*Ny)
-y_orig_meshV = np.reshape(y_orig_mesh,Nx*Ny)
+x_orig_meshV = np.reshape(x_orig_mesh,Nx*Ny) # original x coordinates from mesh in vector form for griddata interpolation
+y_orig_meshV = np.reshape(y_orig_mesh,Nx*Ny) # original y coordinates from mesh in vector form for griddata interpolation
 
-x_us_vec = np.linspace(0,Nx, num = Nx*sample_factor)
-y_us_vec = np.linspace(0,Ny, num = Ny*sample_factor)
+x_us_vec = np.linspace(0,Nx, num = Nx*sample_factor) # generate upsampled x coordinates (here based on pixels)
+y_us_vec = np.linspace(0,Ny, num = Ny*sample_factor) # generate upsampled y coordinates (here based on pixels)
 
 # create mesh of coordinates for plotting
-x_us_mesh,y_us_mesh = np.meshgrid(x_us_vec,y_us_vec)
+x_us_mesh,y_us_mesh = np.meshgrid(x_us_vec,y_us_vec) # upsampled original coordinates in grid form
 
-Ny_us, Nx_us = x_us_mesh.shape
+Ny_us, Nx_us = x_us_mesh.shape # shape (rows, vectors) of upsampled image
 
-x_us_meshV = np.reshape(x_us_mesh,Nx_us*Ny_us)
-y_us_meshV = np.reshape(y_us_mesh,Nx_us*Ny_us)
-
-# --- allocate memory for sorted variables ---
-#im_array = np.zeros((2*Ny,2*Nx))
+x_us_meshV = np.reshape(x_us_mesh,Nx_us*Ny_us) # upsampled original x coordinates from mesh in vector form for griddata interpolation
+y_us_meshV = np.reshape(y_us_mesh,Nx_us*Ny_us) # upsampled original y coordinates from mesh in vector form for griddata interpolation
 
 # interpolate image to upsampled grid
 print('Upsampling image')
+# upsample image using cubic grid interpolation
 img_us_ref = griddata((x_orig_meshV,y_orig_meshV), imarray, (x_us_mesh, y_us_mesh), method ='cubic')
 
+# plot diagnostic figures - original resolution reference image and upsampled reference image
 fig1 = plt.figure() # create a figure with the default size 
 plt.pcolor(img_ref, cmap = 'grey')
 plt.title('291 X 301')
@@ -89,26 +86,26 @@ plt.clim([0, 255])
 
 #%%  define displacement fields and interpolate                       
 # define displacement fields
-#define constant displacement
-x_def_const = 50
-y_def_const = 0
+x_def_const = 50 #define constant x displacement
+y_def_const = 0 #define constant y displacement
 
-x_us_mesh_def = x_us_mesh - x_def_const
-y_us_mesh_def = y_us_mesh - y_def_const
+x_us_mesh_def = x_us_mesh - x_def_const # shift the original upsampled x coordinates by the prescribed deformation
+y_us_mesh_def = y_us_mesh - y_def_const # shift the original upsampled y coordinates by the prescribed deformation
 
-x_us_mesh_defV = np.reshape(x_us_mesh_def,Nx_us*Ny_us)
-y_us_mesh_defV = np.reshape(y_us_mesh_def,Nx_us*Ny_us)
+x_us_mesh_defV = np.reshape(x_us_mesh_def,Nx_us*Ny_us) # deformed x coordinates from mesh in vector form for griddata interpolation 
+y_us_mesh_defV = np.reshape(y_us_mesh_def,Nx_us*Ny_us) # deformed y coordinates from mesh in vector form for griddata interpolation
 
-img_us_refV = np.reshape(img_us_ref,Nx_us*Ny_us)
+img_us_refV = np.reshape(img_us_ref,Nx_us*Ny_us) # reshape upsampled reference image for griddata interpolation
 
 print('Interpolating to deformed positions')
 # interpolate image to upsampled grid
 img_us_def = griddata((x_us_mesh_defV,y_us_mesh_defV), img_us_refV, (x_us_mesh, y_us_mesh), method ='cubic')
 
+# plot diagnostic figures - upsampled reference image and upsampled deformed image
 fig1 = plt.figure() # create a figure with the default size 
 ax1 = fig1.add_subplot(2,2,1) 
 f1 = ax1.pcolor(img_us_ref)
-ax1.set_title('291 X 301')
+ax1.set_title('2910 X 3010')
 fig1.colorbar(f1, ax=ax1)
 
 fig2 = plt.figure()
@@ -138,8 +135,10 @@ for i in range(0,Ny):
             print(ind_col1)
             print(ind_col2)
         
+        # average grey levels in upsampled image over upsampled window size (sample_factor*sample_factor) to return image to original resolution
         img_def[i,j] = np.mean(img_us_def[ind_row1:ind_row2,ind_col1:ind_col2])
 
+# diagnostic figure - downsampled deformed image
 fig3 = plt.figure()
 plt.pcolor(img_def, cmap = 'gray')
 plt.title('291 x 301 - deformed')
