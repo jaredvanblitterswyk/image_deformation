ae6c53c4d307c43ea78fabd2467c27dba5ee521e
Jared Van Blitterswyk <jared.vanblitterswyk@nist.gov>
2020-02-14 18:27:53 -0500

    Added framework to allow multiple images to be generated and saved - works only for 1st image deformed

--- f_generate_deformed_image_stack.py
+++ f_generate_deformed_image_stack.py
@@ -10,18 +10,23 @@ from scipy.interpolate import interp2d, griddata
 
 # load in displacement fields and generate image stack stored in img_def
 
-def deform_images(img_us_ref,coords_us,coords_ref,disp):
-    # create deformed coordinate matrices based on prescribed displacement fields      
-    x_us_mesh_def = coords_us.x_mesh - disp.x # shift the original upsampled x coordinates by the prescribed deformation
-    y_us_mesh_def = coords_us.y_mesh - disp.y # shift the original upsampled y coordinates by the prescribed deformation
-    
-    x_us_mesh_defV = np.reshape(x_us_mesh_def,coords_us.Nx*coords_us.Ny) # deformed x coordinates from mesh in vector form for griddata interpolation 
-    y_us_mesh_defV = np.reshape(y_us_mesh_def,coords_us.Nx*coords_us.Ny) # deformed y coordinates from mesh in vector form for griddata interpolation
-    
-    img_us_refV = np.reshape(img_us_ref,coords_us.Nx*coords_us.Ny) # reshape upsampled reference image for griddata interpolation
-    
-    print('Interpolating to deformed positions')
-    # interpolate image to upsampled grid
-    img_us_def = griddata((x_us_mesh_defV,y_us_mesh_defV), img_us_refV, (coords_us.x_mesh, coords_us.y_mesh), method ='cubic') # cubic interpolation
-    
-    return img_us_def
+def deform_images(img_us_ref,coords_us,coords_ref,disp,num_def_steps):
+    for i in range(0,num_def_steps):
+        print('Image: '+str(i+1)+' of '+str(num_def_steps)+'...')
+        
+        img_us_def = np.zeros((coords_us.Ny,coords_us.Nx,num_def_steps))
+        
+        # create deformed coordinate matrices based on prescribed displacement fields      
+        x_us_mesh_def = coords_us.x_mesh - disp.x[:,:,i] # shift the original upsampled x coordinates by the prescribed deformation
+        y_us_mesh_def = coords_us.y_mesh - disp.y[:,:,i] # shift the original upsampled y coordinates by the prescribed deformation
+        
+        x_us_mesh_defV = np.reshape(x_us_mesh_def,coords_us.Nx*coords_us.Ny) # deformed x coordinates from mesh in vector form for griddata interpolation 
+        y_us_mesh_defV = np.reshape(y_us_mesh_def,coords_us.Nx*coords_us.Ny) # deformed y coordinates from mesh in vector form for griddata interpolation
+        
+        img_us_refV = np.reshape(img_us_ref,coords_us.Nx*coords_us.Ny) # reshape upsampled reference image for griddata interpolation
+        
+        #print('Interpolating to deformed positions')
+        # interpolate image to upsampled grid
+        img_us_def[:,:,i] = griddata((x_us_mesh_defV,y_us_mesh_defV), img_us_refV, (coords_us.x_mesh, coords_us.y_mesh), method ='cubic') # cubic interpolation
+        
+        return img_us_def
--- m_deform_images.py
+++ m_deform_images.py
@@ -27,21 +27,28 @@ Material Measurement Laboratory
 import os
 from os import listdir
 import numpy as np; import math as m
+import matplotlib as matplib
 from matplotlib import pyplot as plt
 from PIL import Image
 import pandas as pd
 import numpy.matlib
 from scipy.interpolate import interp2d, griddata
+import scipy
 from f_generate_deformed_image_stack import deform_images
 
-os.chdir(r"Z:\Python\image_deformation\sample_speckle_images")
+os.chdir(r"Z:\Python\image_deformation")
 # varify the path using getcwd() 
 cwd = os.getcwd() 
 
-# ---------------------- load in reference image ------------------------------
-filename = cwd+'/hc_reference_dic_challenge_1.tif' # hardcode the location of the figure
+image_dir = 'Z:/Python/image_deformation/rigid_body_translation' # directory where main images will be stored
 
-img_ref = Image.open(filename) # open image
+image_name_prefix = 'hc_dic_' # prefix of image name - will be followed by descriptor (i.e: rigid body tranlsation of 1 pixel in x, 0 in y would be image_name_prefix_10x_00y)
+
+image_name_ref = image_name_prefix+'00x_00y.tif' # name of reference image
+# ---------------------- load in reference image ------------------------------
+#filename = image_dir+'/hc_reference_dic_challenge_1.tif' # hardcode the location of the figure
+
+img_ref = Image.open(image_dir+'/'+image_name_ref) # open image
 '''
 # for rgb images
 def rgb2gray(rgb):
@@ -79,11 +86,12 @@ x_us_meshV = np.reshape(x_us_mesh,Nx_us*Ny_us) # upsampled original x coordinate
 y_us_meshV = np.reshape(y_us_mesh,Nx_us*Ny_us) # upsampled original y coordinates from mesh in vector form for griddata interpolation
 
 # interpolate image to upsampled grid
-print('Upsampling image')
+print('Upsampling reference image...')
 # upsample image using cubic grid interpolation
 img_us_ref = griddata((x_orig_meshV,y_orig_meshV), imarray, (x_us_mesh, y_us_mesh), method ='cubic')
-
+print('Complete.')
 # plot diagnostic figures - original resolution reference image and upsampled reference image
+'''
 fig1 = plt.figure() # create a figure with the default size 
 plt.pcolor(img_ref, cmap = 'gray')
 plt.title('original resolution reference image')
@@ -95,17 +103,25 @@ plt.pcolor(img_us_ref, cmap = 'gray')
 plt.title('upsampled reference image')
 plt.colorbar()
 plt.clim([0, 255])
+'''
 
 #%%  define displacement fields and interpolate                       
-# --------------------- define displacement fields ----------------------------
-'''
-x_def = 10 #define constant x displacement
-y_def = 0 #define constant y displacement
-'''
+# ----------------- define rigid-body displacement fields ---------------------
+# sub-pixel shift up to 1 pixel
+num_def_steps = 2
+rows,cols = img_us_ref.shape
+x_def = np.zeros((rows,cols,num_def_steps))
+y_def = np.zeros((rows,cols,num_def_steps))
 
+for i in range(0,num_def_steps):
+    print((i+1)/num_def_steps)
+    x_def[:,:,i] = (i+1)/num_def_steps*np.ones((rows,cols))
+    
+
+'''
 # ----------------------- define distortion field -----------------------------
 # d^-1(x,y) = (1+p(x^2 + y^2))(1+pr^2)(x',y')
-'''
+
 x_def = np.zeros(img_us_ref.shape)
 y_def = np.zeros(img_us_ref.shape)
 
@@ -115,7 +131,7 @@ y_us_mesh_c = y_us_mesh-0.5*max(y_us_mesh[:,0])
 
 # distortion coefficients
 # create approx. 1 pixel displacement radial distortion in x and y
-k1 = -1.76e-10 # based on Table 1 in B. Pan et al. Systematic errors in two-dimensional digital image correlation due to lens distortion, 2013 - adjusted for pixels instead of physical dimensions
+k1 = -1.76e-12 # based on Table 1 in B. Pan et al. Systematic errors in two-dimensional digital image correlation due to lens distortion, 2013 - adjusted for pixels instead of physical dimensions
 k2 = 0
 
 for i in range(0,Ny_us):
@@ -127,6 +143,7 @@ for i in range(0,Ny_us):
         y_def[i,j] = -1*y_us_mesh_c[i][j]*(k1*r**2 + k2*r**4) # inverse y displacement caused by distortion     
 '''
 # ---------------------- define displacement field ----------------------------
+'''
 x_def = 0.5*np.sin(2*m.pi*x_us_mesh/Nx)
 y_def = np.zeros(img_us_ref.shape)
 
@@ -140,17 +157,13 @@ fig2 = plt.figure()
 plt.pcolor(y_def, cmap = 'gray')
 plt.title('upsampled y-displacement field')
 plt.colorbar()
+'''
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
-# --------------------------- create classes ----------------------------------
-# store variables in classes to pass to functions
-# an empty class of upsampled coordinates
 class coords_us:
     pass
-
 coords_us = coords_us()
 
-# create coordinate data frame 
 coords_us.x_mesh = x_us_mesh
 coords_us.y_mesh = y_us_mesh
 coords_us.x = x_us_vec
@@ -158,13 +171,10 @@ coords_us.y = y_us_vec
 coords_us.Nx = Nx_us
 coords_us.Ny = Ny_us
 
-# an empty class of original coordinates
 class coords_ref:
     pass
 
 coords_ref = coords_ref()
-
-# create coordinate data frame 
 coords_ref.x_mesh = x_orig_mesh
 coords_ref.y_mesh = y_orig_mesh
 coords_ref.x = x_orig_vec
@@ -180,7 +190,24 @@ disp = disp()
 disp.x = x_def
 disp.y = y_def
 
-img_us_def = deform_images(img_us_ref,coords_us,coords_ref,disp)
+print('Numerically deforming images...')
+img_us_def = deform_images(img_us_ref,coords_us,coords_ref,disp,num_def_steps)
+print('Complete.')
+
+# define strings describing displacements for each image
+disp_inc_str_x = ["{0:02}".format(int(np.mean(np.mean(disp.x[:,:,i]*10)))) for i in range(0,num_def_steps)]
+disp_inc_str_y = ["{0:02}".format(int(np.mean(np.mean(disp.y[:,:,i]*10)))) for i in range(0,num_def_steps)]
+
+# save images to file
+print('Saving images to file...')
+for i in range(0,num_def_steps):
+    print('Image: '+str(i+1)+' of '+str(num_def_steps)+'...')
+    filename = image_dir+'/'+image_name_prefix+disp_inc_str_x[i]+'x_'+disp_inc_str_y[i]+'y.tiff'
+    print(filename)
+    
+    im = Image.fromarray(img_us_def[:,:,i].astype(np.uint8))
+    im.save(filename)
+
 '''
 # create deformed coordinate matrices based on prescribed displacement fields      
 x_us_mesh_def = x_us_mesh - x_def # shift the original upsampled x coordinates by the prescribed deformation
@@ -196,6 +223,7 @@ print('Interpolating to deformed positions')
 img_us_def = griddata((x_us_mesh_defV,y_us_mesh_defV), img_us_refV, (x_us_mesh, y_us_mesh), method ='cubic')
 '''
 # plot diagnostic figures - upsampled reference image and upsampled deformed image
+'''
 fig1 = plt.figure()
 plt.pcolor(img_us_ref, cmap = 'gray')
 plt.title('upsampled reference image')
@@ -207,6 +235,7 @@ plt.pcolor(img_us_def, cmap = 'gray')
 plt.title('upsampled deformed image')
 plt.colorbar()
 plt.clim([0, 255])
+'''
 
 #%% down-sample to original image resolution
 img_def = np.zeros((Ny,Nx))
